9d63ad8f7cb3b85e96040de50a4a86e73af05346
Savic <veljko.savic@elfak.rs>
2025-07-26 00:01:23 +0200

    Zapoceta lista za challengi

--- Aplikacija/backend/Controllers/ChallengeController.cs
+++ Aplikacija/backend/Controllers/ChallengeController.cs
@@ -1,9 +1,40 @@
+using backend.DTOs;
 namespace backend.Controllers;
+
 
 [Route("api/[controller]")]
 [ApiController]
 [Authorize]
-public class ChallengeController() : ControllerBase
+public class ChallengeController : ControllerBase
 {
+    public ApplicationDbContext Context { get; set; }
+
+    public ChallengeController(ApplicationDbContext context)
+    {
+        Context = context;
+    }
+
+    [HttpGet("GetChallenges")]
+    public async Task<ActionResult<IEnumerable<ChallengeDto>>> GetChallenges()
+    {
+        var challenges = await Context.Challenges
+            .Include(c => c.Category)
+            //.Include(c => c.User)
+            .Select(c => new ChallengeDto
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Description = c.Description,
+                //IsArchieved = c.Archieved,
+                IsPublic = c.Public,
+                Points = c.Points,
+                CreatedAt = c.CreatedAt,
+                //AutorName = c.User.Username,
+                CategoryName = c.Category.Name
+            })
+            .ToListAsync();
+
+        return Ok(challenges);
+    }
 
 }
--- Aplikacija/frontend/src/pages/Challenges/Challenges.tsx
+++ Aplikacija/frontend/src/pages/Challenges/Challenges.tsx
@@ -1,9 +1,41 @@
 import './Challenges.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface Challenge {
+    id: number;
+    name: string;
+    categoryName: string;
+    createdAt: string;
+}
 
 function Challenges() {
-    return <>
-        TODO: Challenges
-    </>
+    const [challenges, setChallenges] = useState<Challenge[]>([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5072/api/Challenge/GetChallenges', { ////////ispravi
+            headers: {Accept: 'application/json'}
+        })
+        
+            .then(response => {
+        console.log('Odgovor sa servera:', response.data);
+        setChallenges(response.data);})
+            .catch(error => console.error('Greška pri dohvatanju izazova:', error));
+    }, []);
+
+    return (
+        <div>
+            <h2>Lista Izazova</h2>
+            <ul>
+                {challenges.map(challenge => (
+                    <li key={challenge.id}>
+                        <strong>{challenge.name}</strong> – {challenge.categoryName} – {new Date(challenge.createdAt).toLocaleDateString()}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
+
 
 export default Challenges;
