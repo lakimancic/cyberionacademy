892f42a14f9e0152ae8eec18a33bbd9fec8641ce
mancic.lazar <mancic.lazar@elfak.rs>
2025-08-04 23:04:44 +0200

    Kreiranje challenga dizajn

--- Aplikacija/frontend/src/pages/Challenges/ChallengeStudio.tsx
+++ Aplikacija/frontend/src/pages/Challenges/ChallengeStudio.tsx
@@ -1,9 +1,174 @@
+import { useEffect, useMemo, useState } from 'react';
 import './ChallengeStudio.css';
+import DataTable from '@/components/Table/DataTable';
+import difficulties from '@/utils/difficulties';
+import { MenuItem, Rating, Select } from '@mui/material';
+import api from '@/lib/api';
+import SearchBar from '@/components/SearchBar/SearchBar';
+import { useNavigate } from 'react-router-dom';
+
+interface Challenge {
+    id: number;
+    name: string;
+    categoryName: string;
+    points: number;
+    averageRating: number;
+    solvedCount: number;
+    isArchived: boolean;
+    isPublic: boolean;
+    avatarUrl?: string;
+    difficulty: number;
+};
+
+type SortKey = 'name' | 'points' | 'categoryName';
 
 function ChallengeStudio() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [challenges, setChallenges] = useState<Challenge[]>([]);
+    const [searchWord, setSearchWord] = useState('');
+    const [sortKey, setSortKey] = useState<SortKey>('name');
+    const [sortDir, setSortDir] = useState<'asc'|'desc'>('asc');
+    const [selectedCategory, setSelectedCategory] = useState<string>('');
+    const [selectedDifficulty, setSelectedDifficulty] = useState<string>('');
+    const [categories, setCategories] = useState<string[]>([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        api.get('/Challenge/GetCategories')
+            .then(res => setCategories(res.data))
+            .catch(err => console.error('Greška pri dohvatanju kategorija', err));
+    }, []);
+
+    const fetchChallenges = (searchQuery?: string) => {
+        const params = {
+            sortKey,
+            sortDirection: sortDir,
+            page: currentPage,
+            search: searchQuery,
+            category: selectedCategory !== '' ? selectedCategory : undefined,
+            difficulty: selectedDifficulty !== '' ? Number(selectedDifficulty) : undefined
+        };
+
+        api.get('/Challenge/GetChallenges', { params })
+            .then(response => {
+                setChallenges(response.data.items);
+                setTotalPages(response.data.totalPages);
+            })
+            .catch(error => console.error('Greška pri dohvatanju izazova:', error));
+    };
+
+    const onSearch = () => {
+        fetchChallenges(searchWord);
+    };
+
+    useEffect(() => {
+        fetchChallenges();
+    }, [currentPage, sortKey, sortDir, selectedCategory, selectedDifficulty]);
+
+    const mappedChallenges = useMemo(() => {
+        return challenges.map(chal => {
+            return {
+                name: <div className='challenge-name'>
+                    <strong>{chal.name}</strong>
+                    <div className="difficulty">{(difficulties as any)[chal.difficulty] ?? 'Unknown'}</div>
+                </div>,
+                categoryName: chal.categoryName,
+                points: chal.points,
+                rating: <Rating 
+                    value={chal.averageRating}
+                    precision={0.1}
+                    readOnly
+                    size="small"
+                />,
+                sovleCount: chal.solvedCount,
+                visible: chal.isPublic ? 'Public' : 'Private',
+                status: chal.isArchived ? 'Archived' : 'Active',
+                id: chal.id
+            }
+        })
+    }, [challenges]);
+
     return (
         <div className="challenge-studio">
-            <h2>Create new or edit your Challenges</h2>
+            <h2>Create new or edit Challenges</h2>
+
+            <div className="challenge-studio-filters">
+                <button
+                    className='challenge-studio-add'
+                    onClick={() => {
+                        navigate("/moderator/new-challenge")
+                    }}
+                >Add New Challenge</button>
+                <div className="challenge-studio-right">
+                    <SearchBar 
+                        label='Challenges'
+                        searchWord={searchWord}
+                        setSearchWord={setSearchWord}
+                        onSearch={onSearch}
+                    />
+                    <Select
+                        value={selectedCategory}
+                        displayEmpty
+                        onChange={e => setSelectedCategory(e.target.value)}
+                        renderValue={selected => {
+                            if(selected.length === 0)
+                                return <span className='admin-placeholder'>Filter by Category</span>;
+
+                            return selected;
+                        }}
+                        >
+                            <MenuItem value=''>All Categories</MenuItem>
+                            {categories.map((cat, idx) => (<MenuItem key={idx} value={cat}>
+                                {cat}
+                            </MenuItem>))}
+                    </Select>
+                    <Select
+                        value={selectedDifficulty}
+                        displayEmpty
+                        onChange={e => setSelectedDifficulty(e.target.value)}
+                        renderValue={selected => {
+                            if(selected.length === 0)
+                                return <span className='admin-placeholder'>Filter by Difficulty</span>;
+
+                            return difficulties[Number(selected)];
+                        }}
+                        >
+                            <MenuItem value=''>All Categories</MenuItem>
+                            {difficulties.map((diff, idx) => (<MenuItem key={idx} value={idx}>
+                                {diff}
+                            </MenuItem>))}
+                    </Select>
+                </div>
+            </div>
+
+            <DataTable 
+                className='challenge-studio-table'
+                data={mappedChallenges}
+                columns={[
+                    { key: 'name', header: 'Challenge', sortable: true },
+                    { key: 'categoryName', header: 'Category', sortable: true },
+                    { key: 'points', header: 'Points', sortable: true },
+                    { key: 'rating', header: 'Avg. Rating' },
+                    { key: 'sovleCount', header: 'Users Solves' },
+                    { key: 'visible', header: 'Visibility' },
+                    { key: 'status', header: 'Status' }
+                ]}
+                pagination={{
+                    page: currentPage,
+                    totalPages: totalPages,
+                    setPage: setCurrentPage
+                }}
+                sort={{
+                    key: sortKey,
+                    dir: sortDir,
+                    onSetSortDir: setSortDir,
+                    onSetSortKey: arg => setSortKey(arg as SortKey)
+                }}
+                onRowClick={row => {
+                    navigate(`/moderator/edit-challenge/${row.id}`);
+                }}
+            />
         </div>
     )
 }
--- Aplikacija/frontend/src/pages/Challenges/CreateChallenge.tsx
+++ Aplikacija/frontend/src/pages/Challenges/CreateChallenge.tsx
@@ -1,9 +1,213 @@
+import { useParams } from 'react-router-dom';
+import InputField from '@/components/Auth/InputField';
 import './CreateChallenge.css';
+import { FormControlLabel, MenuItem, Select, Slider, Switch } from '@mui/material';
+import difficulties from '@/utils/difficulties';
+import { useEffect, useState } from 'react';
+import api from '@/lib/api';
+import { MdCloudUpload } from 'react-icons/md';
+import { FaPlus, FaTrashAlt } from 'react-icons/fa';
+import { IoIosSave, IoMdBuild } from 'react-icons/io';
+
+const getColor = (val: number) => {
+    if (val < 3) return 'success.main';
+    if (val < 7) return 'warning.main';
+    return 'error.main';
+};
+
+const getColorHex = (val: number) => {
+    if (val < 3) return '#66bb6a';
+    if (val < 7) return '#ffa726';
+    return '#f44336';
+};
+
+const pointsValues = [
+    { start: 50, step: 10 },
+    { start: 100, step: 10 },
+    { start: 150, step: 10 },
+    { start: 200, step: 20 },
+    { start: 300, step: 20 },
+    { start: 400, step: 20 },
+    { start: 500, step: 20 },
+    { start: 600, step: 50 },
+    { start: 850, step: 50 },
+    { start: 1100, step: 100 }
+];
 
 function CreateChallenge() {
-    return <>
-        CREATE CHALLENGE
-    </>
+    const [diffValue, setDiffValue] = useState(0);
+    const [points, setPoints] = useState(50);
+    const params = useParams();
+    const [selectedCategory, setSelectedCategory] = useState<string>('');
+    const [categories, setCategories] = useState<string[]>([]);
+    
+    useEffect(() => {
+        api.get('/Challenge/GetCategories')
+            .then(res => setCategories(res.data))
+            .catch(err => console.error('Greška pri dohvatanju kategorija', err));
+    }, []);
+
+    return (
+        <div className="challenge-create">
+            <h1>{params.id ? 'Edit' : 'Create'} Challenge</h1>
+            <form className="challenge-create-form">
+                <h2>Challenge Information</h2>
+                <div className="challenge-create-col">
+                    <InputField
+                        type='text'
+                        label='Name'
+                        handleChange={() => {}}
+                        // error={basicForm.formState.errors.username?.message}
+                        // inputProps={{...basicForm.register('username')}}
+                    />
+                    <div className="form-field challenge-create-desc">
+                        <div className="form-label">Description</div>
+                        <textarea
+                            className={/*basicForm.formState.errors.bio*/false ? 'form-input-error' : 'form-input-normal'}
+                            spellCheck={false}
+                            // onKeyUp={() => handleBasicChange('bio')}
+                            // {...basicForm.register('bio')}
+                        ></textarea>
+                        {/* <div className={`form-error ${basicForm.formState.errors.bio ? '' : 'form-hidden'}`}>{basicForm.formState.errors.bio?.message ?? ''}</div> */}
+                    </div>
+                    <InputField
+                        type='text'
+                        label='Flag'
+                        handleChange={() => {}}
+                        // error={basicForm.formState.errors.username?.message}
+                        // inputProps={{...basicForm.register('username')}}
+                    />
+                </div>
+                <div className="challenge-create-col">
+                    <div className="form-field">
+                        <div className="form-label">Difficulty</div>
+                        <Slider
+                            value={diffValue}
+                            onChange={(_, val) => {
+                                setDiffValue(val);
+                                setPoints(pointsValues[val].start);
+                            }}
+                            aria-label="Difficulty"
+                            defaultValue={30}
+                            getAriaValueText={val => difficulties[val]}
+                            valueLabelFormat={val => difficulties[val]}
+                            valueLabelDisplay="auto"
+                            shiftStep={3}
+                            step={1}
+                            marks
+                            min={0}
+                            max={9}
+                            sx={{
+                                color: getColor(diffValue)
+                            }}
+                        />
+                        <div 
+                            className="form-value-show"
+                            style={{
+                                color: getColorHex(diffValue)
+                            }}
+                        >{difficulties[diffValue]}</div>
+                    </div>
+                    <div className="form-field">
+                        <div className="form-label">Points</div>
+                        <Slider
+                            value={points}
+                            onChange={(_, val) => setPoints(val)}
+                            aria-label="Points"
+                            defaultValue={50}
+                            valueLabelDisplay="auto"
+                            shiftStep={pointsValues[diffValue].step * 3}
+                            step={pointsValues[diffValue].step}
+                            marks
+                            min={pointsValues[diffValue].start}
+                            max={pointsValues[diffValue].start + pointsValues[diffValue].step * 4}
+                        />
+                        <div className="form-value-show">{points}pts</div>
+                    </div>
+                    <div className="challenge-create-switches">
+                        <div className="challenge-create-col form-switch-con">
+                            <div className="form-label">Visibility</div>
+                            <FormControlLabel control={<Switch defaultChecked />} label="Label" />
+                        </div>
+                        <div className="challenge-create-col form-switch-con">
+                            <div className="form-label">Status</div>
+                            <FormControlLabel control={<Switch defaultChecked />} label="Label" />
+                        </div>
+                    </div>
+                    <div className="form-field">
+                        <div className="form-label">Category</div>
+                        <Select
+                            value={selectedCategory}
+                            displayEmpty
+                            onChange={e => setSelectedCategory(e.target.value)}
+                            renderValue={selected => {
+                                if(selected.length === 0)
+                                    return <span className='admin-placeholder'>Select Category</span>;
+
+                                return selected;
+                            }}
+                            >
+                                {categories.map((cat, idx) => (<MenuItem key={idx} value={cat}>
+                                    {cat}
+                                </MenuItem>))}
+                        </Select>
+                    </div>
+                </div>
+                <h2>Upload Information</h2>
+                <div className="challenge-create-col">
+                    <div className="form-field">
+                        <div className="form-label">Docker Zip File (Containing Dockerfile)</div>
+                        <div className="challenge-upload-con">
+                            <button type="button"><MdCloudUpload />  Upload</button>
+                            <p className='challenge-no-upload'>No file upload</p>
+                        </div>
+                        <input
+                            type="file"
+                            // ref={fileInputRef}
+                            style={{ display: 'none' }}
+                            accept=".zip"
+                            // onChange={handleFileChange}
+                        />
+                    </div>
+                </div>
+                <div className="challenge-create-col">
+                    <div className="form-field">
+                        <div className="form-label">Challenge Zip File (Available for user)</div>
+                        <div className="challenge-upload-con">
+                            <button type="button"><MdCloudUpload />  Upload</button>
+                            <p className='challenge-no-upload'>No file upload</p>
+                        </div>
+                        <input
+                            type="file"
+                            // ref={fileInputRef}
+                            style={{ display: 'none' }}
+                            accept=".zip"
+                            // onChange={handleFileChange}
+                        />
+                    </div>
+                </div>
+                <div className="challenge-create-buttons">
+                    <button type="button" className='challenge-btn-add'><FaPlus /> Create</button>
+                    <button type="button" className='challenge-btn-save'><IoIosSave /> Save Changes</button>
+                    <button type="button" className='challenge-btn-del'><FaTrashAlt /> Delete</button>
+                </div>
+            </form>
+            <div className="challenge-create-con">
+                <h2>Docker Image Builder</h2>
+                <div className="challenge-docker-con">
+                    <h3>No docker image built</h3>
+                    <div className="challenge-docker-buttons">
+                        <button type="button" className='challenge-btn-add'><IoMdBuild /> Build</button>
+                        <button type="button" className='challenge-btn-del'><FaTrashAlt /> Delete</button>
+                    </div>
+                    <div className="form-label">Docker build Log</div>
+                    <div className="challenge-docker-logs">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default CreateChallenge;
